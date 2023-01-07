9444c7bc965a6ac185f2b63177c617f10ac22d4e
nexus1010 <bot.developer3@gmail.com>
2023-01-07 11:50:19 -0600

    Fixed

--- server.js
+++ server.js
@@ -6,9 +6,17 @@ const mongoose = require("mongoose");
 const recipesRouter = require("./routes/recipes-route");
 const port = process.env.PORT || 3200;
 
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true
 })
+
+app.use(bodyParser.json());
+app.use(cors());
+app.options("*", cors());
+
 
 const db = mongoose.connection;
 app.use(express.json())
